ab4945f4dda30679e1dbce4807497a79f02df324
Andrew Tenno <andrew.tenno@gmail.com>
2018-03-19 22:22:08 -0700

    Adds basic view model generation

---
+++ ThriveMarket-Test/View Model/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  ThriveMarket-Test
+//
+//  Created by Andrew Tenno on 3/19/18.
+//  Copyright © 2018 Andrew Tenno. All rights reserved.
+//
+
+import Foundation
+
+struct PostViewModel {
+    let title: String
+    let thumbnail: URL?
+}
---
+++ ThriveMarket-Test/View Model/ViewModelGenerator.swift
@@ -0,0 +1,52 @@
+//
+//  ViewModelGenerator.swift
+//  ThriveMarket-Test
+//
+//  Created by Andrew Tenno on 3/19/18.
+//  Copyright © 2018 Andrew Tenno. All rights reserved.
+//
+
+import Foundation
+
+enum ViewModelGenerationResult {
+    case success([PostViewModel], String)
+    case failure(Error)
+}
+
+protocol ViewModelGeneratable {
+    func fetchViewModels(afterPage page: String?, completion: @escaping (ViewModelGenerationResult) -> Void)
+}
+
+class ViewModelGenerator {
+    let resultsFetcher: ResultsFetchable
+
+    init(resultsFetcher: ResultsFetchable) {
+        self.resultsFetcher = resultsFetcher
+    }
+}
+
+extension ViewModelGenerator: ViewModelGeneratable {
+    func fetchViewModels(afterPage page: String?, completion: @escaping (ViewModelGenerationResult) -> Void) {
+        resultsFetcher.fetchListings(afterPage: page) { (result) in
+            handleFetchListings(result: result, completion: completion)
+        }
+    }
+}
+
+private func handleFetchListings(result: ResultsFetchResult, completion: (ViewModelGenerationResult) -> Void) {
+    switch result {
+    case .success(let listing):
+        let (viewModels, nextPageToFetch) = createViewModels(fromListing: listing)
+        completion(.success(viewModels, nextPageToFetch))
+    case .failure(let error):
+        completion(.failure(error))
+    }
+}
+
+func createViewModels(fromListing listing: Listing) -> ([PostViewModel], String) {
+    return (listing.page.children.map({ (child) -> PostViewModel in
+        let post = child.post
+        return PostViewModel(title: post.title,
+                             thumbnail: post.thumbnail)
+    }), listing.page.after)
+}
